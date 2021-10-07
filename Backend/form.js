758481b3711621108a60e52b766bd3e722cb5f5e
Arya-Wadhwani07 <arya.wadhwani@gmail.com>
2021-10-08 00:15:02 +0530

    Token Verification dummy html made

--- Backend/form.js
+++ Backend/form.js
@@ -22,5 +22,29 @@ const signUp = ()=>{
     alert("Signed Up")
 }
 
+const signIn = ()=>{
+    var email = document.getElementById("email")
+    var password = document.getElementById("password")
+
+    const promise = auth.signInWithEmailAndPassword(email.value, password.value)
+    promise.catch(e => {
+        alert(e.message)
+    })
+    alert("Signed In "+email.value)
+}
+
+const signOut = ()=>{
+    auth.signOut()
+    alert("Signed Out")
+}
+
+const currentUser = ()=>{
+    const user=auth.currentUser
+    const idToken = auth.currentUser.getIdToken(true)
+    alert(user.email)
+    console.log(idToken)
+}
+
+
 
 
