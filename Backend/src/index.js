82ea71a74d849282b8405f34e3c2128b64b1f7f5
Arya-Wadhwani07 <arya.wadhwani@gmail.com>
2021-10-08 16:48:12 +0530

    Added organ routes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,11 +2,13 @@ const express = require('express')
 require('./db/mongoose')
 const {port} = require('../secrets')
 const hospitalRouter = require('./routers/hospital')
+const organRouter = require('./routers/organ')
 
 const app = express()
 
 app.use(express.json())
 app.use(hospitalRouter)
+app.use(organRouter)
 
 app.listen(port,()=>{
     console.log("Server is up at port ",port)
---
+++ Backend/src/routers/organ.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const organ = require('../models/organ')
+const mongoose = require('mongoose')
+const checkUser = require('../middleware/checkUser')
+
+const router = express.Router()
+
+// POST Route to create an organ profile
+router.post('/organ',async(req,res)=>{
+    const Organ = new organ(req.body)
+    try{
+        await Organ.save()
+        res.status(201).send({Organ})
+    } catch(e){
+        res.status(500).send(e)
+    }
+})
+
+// GET Route to get all the organs
+router.get('/organ',async(req,res)=>{
+    try{
+        const organ_data = await organ.find({})
+        res.status(200).send(organ_data)
+    } catch(e){
+        res.status(500).send(e)
+    }
+})
+
+// GET Route to get a particular organ
+router.get('/organ:id',async(req,res)=>{
+    const _id = req.params.id
+    try{
+        const organ_data = await user.find({_id})
+        res.status(200).send(organ_data)
+    } catch(e){
+        res.status(500).send(e)
+    }
+})
+
+// DELETE Route to delete a particular organ
+router.delete('/organ/:id',async(req,res)=>{
+    const _id = req.params.id
+    try{
+        const organ_data = await organ.findOneAndDelete({_id})
+        res.status(200).send(organ_data)
+    } catch(e){
+        res.status(500).send(e)
+    }
+})
+
+// PATCH Route to edit a particular organ
+router.patch('/organ/:id',async(req,res)=>{
+    const _id = req.params.id
+    const updates = req.body
+    try{
+        const updated_value = await organ.findOneAndUpdate({_id},updates,{new:true})
+        res.status(200).send(updated_value)
+    } catch(e){
+        res.status(200).send(e)
+    }
+})
+
+
+module.exports = router
